cb21e2e15fe81d997c95cd9a6baf3682367dc634
GoodSoldier1P1 <danielamyx859@gmail.com>
2023-10-30 13:17:20 -0400

    finished with inputs

--- realty.py
+++ realty.py
@@ -1,26 +1,27 @@
 class Investmentreturn():
-    def __init__(self, income, expenses, coc) -> None:
-        self.income = income
-        self.expenses = expenses
-        self.coc = coc
+    def __init__(self) -> None:
+        self.income = 0
+        self.expenses = 0
+        self.coc = 0
 
 
     def anuals(self):
+        income = input("What is your monthly Income on the property? ")
+        expenses = input("What are your monthly expenses on the property? ")
+        self.coc = int(income) - int(expenses)    # cash flow
+        
+        print(self.coc)
+        a_cf = int(self.coc) * 12          # anual cash flow
 
-        c_flow = self.income - self.expenses    # cash flow
-        print(c_flow)
-        print(self.income)
-        print(self.expenses)
-        a_cf = c_flow * 12          # anual cash flow
-
-        roi = a_cf / self.coc 
+        total_invest = input("What is your total investment? (Down Payment, Closing Cost, etc)")
+        roi = a_cf / int(total_invest)
         percent = roi * float(100)
 
         print(f"Your ROI is {percent}%")
     
     def runner(self):
         while True:
-            options = input("Would you like to know your ROI with the numbers entered? (yes or no)")
+            options = input("Would you like to know what your ROI is in just 3 easy steps? (yes or no) ")
             if (options == 'no'):
                 break
             else:
@@ -29,9 +30,12 @@ class Investmentreturn():
 
 
 
-soldierRealty = Investmentreturn(2000,1610,50000)
+soldierRealty = Investmentreturn()
 soldierRealty.runner()
 
-# print(soldierRealty.income)
-# print(soldierRealty.expenses)
-# print(soldierRealty.coc)
+
+
+### For this Example
+# Monthly Income == 2,000
+# Monthly Expenses == 1,610
+# Total Investment == 50,000
